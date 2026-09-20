667714e5a128397a12bdea509abd3e92809d1953
nemu33 <nemu36895@gmail.com>
2026-09-20 21:44:04 +0900

    ci: isolate Worker lint from app lint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,6 @@ const expoConfig = require('eslint-config-expo/flat');
 module.exports = [
   ...expoConfig,
   {
-    ignores: ['node_modules/**', '.expo/**', 'coverage/**', '_expo-template/**'],
+    ignores: ['node_modules/**', '.expo/**', 'coverage/**', '_expo-template/**', 'worker/**'],
   },
 ];
